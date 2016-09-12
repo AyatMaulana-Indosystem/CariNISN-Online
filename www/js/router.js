fa1e66a0f224e4c165b042761a7b41f26247a1f8
AyatMaulana <Ayatmaulana.indosystem@gmail.com>
2016-09-12 15:47:07 +0700

    first commit

---
+++ www/js/router.js
@@ -0,0 +1,74 @@
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+    .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+    })
+
+    .state('main', {
+        url : '/main',
+        views : {
+            'menuContent' : {
+                templateUrl : 'templates/main.html'
+            }
+        }
+    })
+
+    .state('app.lists', {
+        url: '/lists',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/lists.html',
+                controller: 'ListsCtrl'
+            }
+        }
+    })
+
+    .state('app.ink', {
+        url: '/ink',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/ink.html',
+                controller: 'InkCtrl'
+            }
+        }
+    })
+
+    .state('app.motion', {
+        url: '/motion',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/motion.html',
+                controller: 'MotionCtrl'
+            }
+        }
+    })
+
+    .state('app.components', {
+        url: '/components',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/components.html',
+                controller: 'ComponentsCtrl'
+            }
+        }
+    })
+
+    .state('app.extensions', {
+        url: '/extensions',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/extensions.html',
+                controller: 'ExtensionsCtrl'
+            }
+        }
+    })
+    ;
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/components');
+});
